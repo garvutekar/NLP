cd9eefecda72dbfaaf2e506b316475d209efdd2d
garvutekar <gxrv20@gmail.com>
2025-05-12 14:02:24 +0530

    Add files via upload

---
+++ moduleA.py
@@ -0,0 +1,5 @@
+def greeting(a):
+    print(f"The greeting is:-{a}")
+
+def salutation(b):
+    print(f"The salutaion is:-{b}")
---
+++ moduleB.py
@@ -0,0 +1,5 @@
+def square(i):
+    return i**2
+
+def cube(j):
+    return j**3
